a446f5b90544a381142824abbe915d94f36f8057
Ross Tweedie <r.tweedie@gmail.com>
2013-04-22 23:25:59 +0100

    Ability to create an Evernote Notebook

--- classes/api/notebook.php
+++ classes/api/notebook.php
@@ -17,40 +17,55 @@ use Evernote\Api\Abstract_Api;
  */
 class Notebook extends Abstract_Api
 {
-    protected static $note_store;
-    
+    protected static $note_store, $notebook;
+
     function __construct( $client = null )
     {
         parent::__construct( $client );
-        
+
         static::$note_store = $this->client->get_note_client();
     }
-    
+
+
+    /**
+     * Forge / create a new note
+     *
+     * @param array $notebook
+     * @return EDAM\Types\Note
+     */
+    function forge( $notebook = null )
+    {
+        static::$notebook = new \EDAM\Types\Notebook( $notebook );
+
+        return static::$notebook;
+    }
+
+
     public function get_list( )
     {
         return self::$note_store->listNotebooks( $this->client->get_access_key() );
     }
-    
+
     public function get( $guid )
     {
         return self::$note_store->getNotebook( $this->client->get_access_key(), $guid );
     }
-    
+
     public function get_default()
     {
         return self::$note_store->getDefaultNotebook( $this->client->get_access_key() );
     }
-    
+
     public function create( $notebook )
     {
         return self::$note_store->createNotebook( $this->client->get_access_key(), $notebook );
     }
-    
+
     public function update( $notebook )
     {
         return self::$note_store->updateNotebook( $this->client->get_access_key(), $notebook );
     }
-        
+
     public function expunge( $guid )
     {
         return self::$note_store->expungeNotebook( $this->client->get_access_key(), $guid );
